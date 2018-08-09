f8bc0a5b68862664a04c4c85e7e88aea3619df14
Roman Ranniew <romanranniew@gmail.com>
2018-08-09 21:48:41 +0200

    Improve scroll and drunk cursor

--- src/fuckers/scroller.js
+++ src/fuckers/scroller.js
@@ -3,15 +3,19 @@ const zenscroll = require('zenscroll')
 
 module.exports = {
     init: function() {
+        deb('scroll')
         this.last_time = Date.now()
+        this.next_trigger_delay = _.random(3000, 10000)
+        this.active = 1
     },
     start: function() {
-        if (Date.now() - this.last_random_x > _.random(3000, 30000)) {
-            zenscroll.toY(document.documentElement.scrollTop + _.random(-1000, 1000), _.random(100, 3000))
+        if (Date.now() - this.last_time > this.next_trigger_delay && this.active) {
+            zenscroll.toY(document.documentElement.scrollTop + _.random(-2000, 2000), _.random(100, 3000))
             this.last_time = Date.now()
+            this.next_trigger_delay = _.random(3000, 30000)
         }
     },
     stop: function() {
-
+        this.active = 0
     },
 }
